882f4bcbe73d36cc538b98e0e4d6f5192b9d272f
DonWacho <Nikolasporras11@gmail.com>
2026-04-13 21:38:19 -0400

    sa

--- Taller2/src/Paquete/Main.java
+++ Taller2/src/Paquete/Main.java
@@ -1,5 +1,9 @@
 package Paquete;
 
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
 
 	public static void main(String[] args) {
--- Taller2/src/Paquete/Poke.java
+++ Taller2/src/Paquete/Poke.java
@@ -1,5 +1,28 @@
 package Paquete;
 
+import java.io.*;
+import java.util.*;
+
 public class Poke {
+	
+	
+	public static void pokemon(String[] a) throws FileNotFoundException{
+		
+		
+		File pokedex = new File("Pokedex.txt");
+		
+		Scanner lector = new Scanner(pokedex);
+		
+		while(lector.hasNextLine()) {
+			String line = lector.hasNextLine();
+			String[] part = line.split(";");
+			
+			
+			
+		}
+		
+		
+		
+	}
 
 }
