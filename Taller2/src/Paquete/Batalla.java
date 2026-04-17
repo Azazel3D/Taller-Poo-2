f197bee4668a9dcfebf5f7c92445ee040fb98abd
Azazel3D <fergarpi005@gmail.com>
2026-04-17 18:20:29 -0400

    tabla de tipo hecha

---
+++ Taller2/src/Paquete/Batalla.java
@@ -0,0 +1,19 @@
+package Paquete;
+
+public class Batalla {
+	
+	private Poke pokemon1;
+	private Poke pokemon2;
+	
+	
+	public Batalla (Poke pokemon1, Poke pokemon2) {
+		
+		this.pokemon1 = pokemon1;
+		this.pokemon2 = pokemon2;
+		
+	}
+	
+	
+	
+
+}
--- Taller2/src/Paquete/Poke.java
+++ Taller2/src/Paquete/Poke.java
@@ -8,14 +8,25 @@ public class Poke {
 	
 	public static void pokemon(String[] a) throws FileNotFoundException{
 		
+		ArrayList<String> listaPokes = new ArrayList<String>();
+		
 		
 		File pokedex = new File("Pokedex.txt");
 		
 		Scanner lector = new Scanner(pokedex);
 		
+		while (lector.hasNextLine()) {
+			
+			String linea = lector.nextLine();
+			
+			listaPokes.add(linea);
+		}
 		
 		
 		
+		//9 == tipo
+		// logica
+		// NOR  FUE  AGU  PLA  ELE  HIE  LUC  VEN  TIE  VOL  PSI  BIC  ROC  FAN  DRA  ACE  SIN  HAD
 		
 	}
 
